ba2a815d3178d633eadc796bbd4a9dabd5f68e4d
KatonaMihaly <katona0607@gmail.com>
2024-05-02 12:34:53 +0200

    start of nsga2 for 100 generation with constraints

--- examples/magnetics/reluctance_machine/optimisation/case_1/nsga2_run.py
+++ examples/magnetics/reluctance_machine/optimisation/case_1/nsga2_run.py
@@ -12,6 +12,7 @@ from pymoo.operators.mutation.pm import PM
 from pymoo.operators.sampling.rnd import FloatRandomSampling
 from pymoo.termination import get_termination
 from pymoo.optimize import minimize
+from pymoo.termination.default import DefaultMultiObjectiveTermination
 
 import calc_torque_avg_rip
 
@@ -20,9 +21,9 @@ if __name__ == '__main__':
         def __init__(self):
             super().__init__(n_var=5,
                              n_obj=2,
-                             n_ieq_constr=1,
+                             n_ieq_constr=0,
                              n_eq_constr=0,
-                             xl=np.array([15, 90,  1, 1, 1.5]),
+                             xl=np.array([15, 90, 1, 1, 1.5]),
                              xu=np.array([25, 150, 4, 4, 2.5]))
 
         def _evaluate(self, x, out, *args, **kwargs):
@@ -30,12 +31,14 @@ if __name__ == '__main__':
 
             out['F'] = [f[0], f[1]]
 
+
     problem = MyProblem()
 
-    class MyRepair(Repair):
-        problem = MyProblem()
 
-        def _do(self, problem, x, **kwargs):
+    class MyRepair(Repair):
+        constrained_problem = MyProblem()
+
+        def _do(self, constrained_problem, x, **kwargs):
             for i in range(len(x)):
                 g = (math.tan(math.radians(x[i][0] / 2)) * (22 - x[i][4]) + x[i][2] + x[i][3]) - 8
                 if g > 0:
@@ -46,8 +49,8 @@ if __name__ == '__main__':
 
 
     algorithm = NSGA2(
-        pop_size=10,
-        n_offsprings=1,
+        pop_size=50,
+        n_offsprings=25,
         sampling=FloatRandomSampling(),
         crossover=SBX(prob=0.9, eta=15),
         mutation=PM(eta=20),
@@ -55,7 +58,13 @@ if __name__ == '__main__':
         repair=MyRepair()
     )
 
-    termination = get_termination("n_gen", 1)
+    termination = DefaultMultiObjectiveTermination(xtol=1e-8,
+                                                   cvtol=1e-6,
+                                                   ftol=0.0025,
+                                                   period=30,
+                                                   n_max_gen=100,
+                                                   n_max_evals=5000
+                                                   )
 
     res = minimize(problem,
                    algorithm,
@@ -66,13 +75,13 @@ if __name__ == '__main__':
 
     F = res.F
     X = res.X
-    print('Execution time: ' + str(res.exec_time / 60) + ' minutes')
+    print('Execution time: ' + str(res.exec_time / 60 / 60) + ' hours')
 
     df = pd.DataFrame({'X1': X[:, 0], 'X2': X[:, 1], 'X3': X[:, 2], 'X4': X[:, 3], 'X5': X[:, 4], 'AVG': F[:, 0],
                        'RIP': F[:, 1]})
     current_file_path = os.path.abspath(__file__)
     folder_path = os.path.dirname(current_file_path)
-    file_path = os.path.join(folder_path, f'results/nsga2_test.csv')
+    file_path = os.path.join(folder_path, f'results/nsga2_cost_p50o25g100.csv')
     df.to_csv(file_path, encoding='utf-8', index=False)
 
     plt.figure(figsize=(7, 5))
