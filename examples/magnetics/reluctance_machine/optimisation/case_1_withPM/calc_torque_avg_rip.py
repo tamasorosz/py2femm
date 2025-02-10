0d84118fffedd01b01fc84351b2f58e9a960097c
sevir <katona0607@gmail.com>
2025-02-10 20:30:30 +0100

    Case1 recalculated.

--- examples/magnetics/reluctance_machine/optimisation/case_1_withPM/calc_torque_avg_rip.py
+++ examples/magnetics/reluctance_machine/optimisation/case_1_withPM/calc_torque_avg_rip.py
@@ -104,7 +104,7 @@ def torque_avg_rip(J0, ang_co, deg_co, bd, bw, bh, bgp, mh, ang_m):
 
     current_file_path = os.path.abspath(__file__)
     folder_path = os.path.dirname(current_file_path)
-    file_path = os.path.join(folder_path, f'results/all_res_avg_case1_20250125.csv')
+    file_path = os.path.join(folder_path, f'results/all_res_avg_case1_20250205.csv')
 
     # Check if the file exists
     file_exists = os.path.isfile(file_path)
--- examples/magnetics/reluctance_machine/optimisation/case_1_withPM/nsga2_run.py
+++ examples/magnetics/reluctance_machine/optimisation/case_1_withPM/nsga2_run.py
@@ -102,7 +102,7 @@ if __name__ == '__main__':
     else:
         os.makedirs('results')
 
-    file_path = os.path.join(folder_path, f'results/nsga2_case1_p100o100g200_var6_20250125.csv')
+    file_path = os.path.join(folder_path, f'results/nsga2_case1_p100o100g200_var6_20250205.csv')
     df.to_csv(file_path, encoding='utf-8', index=False)
 
     folder_path = ['temp_ang', 'temp_avg_rip', 'temp_cog']
