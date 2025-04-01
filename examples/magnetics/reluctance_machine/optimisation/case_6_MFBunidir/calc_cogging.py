97ba758bc9eec61552a942fd1548818415c0fb70
sevir <katona0607@gmail.com>
2025-04-01 09:53:40 +0200

    Case 6 reconstrained

--- examples/magnetics/reluctance_machine/optimisation/case_6_MFBunidir/calc_cogging.py
+++ examples/magnetics/reluctance_machine/optimisation/case_6_MFBunidir/calc_cogging.py
@@ -54,77 +54,79 @@ def cogging(I0, ang_co, deg_co, bd, bw, bh, bgp, mh, ang_m, ang_mp, deg_m, deg_m
         pass
     else:
         os.makedirs('temp_cog')
+    if mh > 1.5:
+        if ang_m != ang_mp:
+            if not (deg_m == 0 and deg_mp == 0):
+                resol = 16
+                e = 15
 
-    if ang_m != ang_mp:
-        if not (deg_m == 0 and deg_mp == 0):
-            resol = 16
-            e = 15
+                for counter, ia in zip(range(0, resol), np.linspace(0, e, resol)):
+                    variables = model.VariableParameters(fold='cog',
+                                                         out='cog',
+                                                         counter=counter,
+                                                         IAp=I0 * math.cos(math.radians(0)),
+                                                         IAn=-I0 * math.cos(math.radians(0)),
+                                                         IBp=I0 * math.cos(math.radians(0 + 120)),
+                                                         IBn=-I0 * math.cos(math.radians(0 + 120)),
+                                                         ICp=I0 * math.cos(math.radians(0 + 240)),
+                                                         ICn=-I0 * math.cos(math.radians(0 + 240)),
+                                                         ang_co=ang_co,
+                                                         deg_co=deg_co * 10,
+                                                         bd=bd,
+                                                         bw=bw,
+                                                         bh=bh,
+                                                         bg=bgp * 0.5 + mh,
+                                                         ia=ia,
+                                                         mh=mh,
+                                                         ang_m=ang_m,
+                                                         ang_mp=ang_mp,
+                                                         deg_m=deg_m,
+                                                         deg_mp=deg_mp)
 
-            for counter, ia in zip(range(0, resol), np.linspace(0, e, resol)):
-                variables = model.VariableParameters(fold='cog',
-                                                     out='cog',
-                                                     counter=counter,
-                                                     IAp=I0 * math.cos(math.radians(0)),
-                                                     IAn=-I0 * math.cos(math.radians(0)),
-                                                     IBp=I0 * math.cos(math.radians(0 + 120)),
-                                                     IBn=-I0 * math.cos(math.radians(0 + 120)),
-                                                     ICp=I0 * math.cos(math.radians(0 + 240)),
-                                                     ICn=-I0 * math.cos(math.radians(0 + 240)),
-                                                     ang_co=ang_co,
-                                                     deg_co=deg_co * 10,
-                                                     bd=bd,
-                                                     bw=bw,
-                                                     bh=bh,
-                                                     bg=bgp * 0.5 + mh,
-                                                     ia=ia,
-                                                     mh=mh,
-                                                     ang_m=ang_m,
-                                                     ang_mp=ang_mp,
-                                                     deg_m=deg_m,
-                                                     deg_mp=deg_mp)
+                    model.problem_definition(variables)
 
-                model.problem_definition(variables)
+                with Pool(16) as p:
+                    res = list(p.map(execute_model, list(range(0, resol))))
 
-            with Pool(16) as p:
-                res = list(p.map(execute_model, list(range(0, resol))))
+                if None in res:
+                    cogging_pp = 1000
 
-            if None in res:
-                cogging_pp = 1000
+                else:
+                    cogging_pp = np.round(np.max(list(res)) - np.min(list(res)), 2)
+
+                    res.clear()  # To make sure that there is no memory leak
+
+                df = pd.DataFrame({'X1': [ang_co], 'X2': [np.round(deg_co * 10, 2)], 'X3': [bd], 'X4': [bw],
+                                   'X5': [bh], 'X6': [np.round(bgp * 0.5 + mh, 2)], 'X7': [mh], 'X8': [ang_m], 'X9': [ang_mp], 'X10': [deg_m],
+                                   'X11': [deg_mp], 'COG': [cogging_pp]})
+
+                current_file_path = os.path.abspath(__file__)
+                folder_path = os.path.dirname(current_file_path)
+                file_path = os.path.join(folder_path, f'results/all_res_cog_case6_20250401_all_variable.csv')
+
+                # Check if the file exists
+                file_exists = os.path.isfile(file_path)
+
+                # Append the DataFrame to the CSV file
+                with open(file_path, 'a', newline='') as f:
+                    df.to_csv(f, header=not file_exists, index=False)
+
+                # Count the number of rows in a separate operation
+                with open(file_path, 'r') as f:
+                    num_rows = sum(1 for _ in f)
+
+                print('COG: ' + f'{cogging_pp}' + ', IND: ' + f'{num_rows}' +
+                      '\n-----------------------------------------------')
+
+                return cogging_pp
 
             else:
-                cogging_pp = np.round(np.max(list(res)) - np.min(list(res)), 2)
-
-                res.clear()  # To make sure that there is no memory leak
-
-            df = pd.DataFrame({'X1': [ang_co], 'X2': [np.round(deg_co * 10, 2)], 'X3': [bd], 'X4': [bw],
-                               'X5': [bh], 'X6': [np.round(bgp * 0.5 + mh, 2)], 'X7': [mh], 'X8': [ang_m], 'X9': [ang_mp], 'X10': [deg_m],
-                               'X11': [deg_mp], 'COG': [cogging_pp]})
-
-            current_file_path = os.path.abspath(__file__)
-            folder_path = os.path.dirname(current_file_path)
-            file_path = os.path.join(folder_path, f'results/all_res_cog_case6_20250327_no_rib.csv')
-
-            # Check if the file exists
-            file_exists = os.path.isfile(file_path)
-
-            # Append the DataFrame to the CSV file
-            with open(file_path, 'a', newline='') as f:
-                df.to_csv(f, header=not file_exists, index=False)
-
-            # Count the number of rows in a separate operation
-            with open(file_path, 'r') as f:
-                num_rows = sum(1 for _ in f)
-
-            print('COG: ' + f'{cogging_pp}' + ', IND: ' + f'{num_rows}' +
-                  '\n-----------------------------------------------')
-
-            return cogging_pp
+                return random.randint(30, 50)
 
         else:
             return random.randint(30, 50)
 
     else:
         return random.randint(30, 50)
-
 
 
